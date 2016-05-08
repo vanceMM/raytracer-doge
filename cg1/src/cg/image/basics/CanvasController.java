a316673397914366d39aa9dcd17b0d38e9365618
Valentin Risch <valentin_risch@hotmail.de>
2016-05-08 19:42:23 +0200

    finishing kdtree implementation, still buggy while drawing the bounding boxes

--- cg1/src/cg/image/basics/CanvasController.java
+++ cg1/src/cg/image/basics/CanvasController.java
@@ -2,39 +2,77 @@ package cg.image.basics;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.canvas.*;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.*;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.image.*;
+import javafx.scene.image.Image;
+
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import static java.awt.image.BufferedImage.TYPE_INT_RGB;
 
 /**
  * Created by valentin on 05/05/16.
  */
 public class CanvasController implements Initializable {
 
+
     @FXML
-    private Canvas imageCanvas;
+    private ImageView view;
 
     @FXML
     private MenuBar menuBar;
 
+    @FXML
+    private Menu menu;
+
+    @FXML
+            private MenuItem save;
+
+    BufferedImage bufferedImage;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        MenuItem save = new MenuItem("Save");
-        MenuItem close = new MenuItem("close");
-        Menu file = new Menu("File");
 
-        menuBar.getMenus().addAll(file);
-        file.getItems().addAll(save, close);
+        bufferedImage = new BufferedImage(640, 480, TYPE_INT_RGB);
+        final WritableRaster raster = bufferedImage.getRaster();
 
-        imageCanvas = new ImageCanvas(640, 480);
-        imageCanvas.paint();
+        for (int i=0; i<bufferedImage.getWidth(); i++) {
+            for (int j=0; j<bufferedImage.getHeight(); j++) {
+                bufferedImage.setRGB(i,j,0x000000);
+            }
+        }
 
+        WritableImage wr = null;
+        if (bufferedImage != null) {
+            wr = new WritableImage(bufferedImage.getWidth(), bufferedImage.getHeight());
+            PixelWriter pw = wr.getPixelWriter();
+            for (int x = 0; x < bufferedImage.getWidth(); x++) {
+                for (int y = 0; y < bufferedImage.getHeight(); y++) {
+                    pw.setArgb(x,y, bufferedImage.getRGB(x, y));
+                }
+            }
+        }
+        view.setPreserveRatio(true);
+        view.setImage(wr);
 
+        initMenu();
     }
+
+    public void initMenu() {
+
+        menuBar.getMenus().addAll(menu);
+        menu.getItems().addAll(save);
+    }
+
 }
--- cg1/src/cg/image/basics/ImageCanvas.java
+++ cg1/src/cg/image/basics/ImageCanvas.java
@@ -1,5 +1,11 @@
 package cg.image.basics;
 
+import javafx.scene.canvas.*;
+import javafx.scene.*;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.paint.*;
+import javafx.scene.paint.Color;
+
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
@@ -12,29 +18,23 @@ import static java.awt.image.BufferedImage.TYPE_INT_RGB;
  * Created by valentin on 05/05/16.
  */
 public class ImageCanvas extends Canvas {
-
+/*
     private BufferedImage img;
-    private int imageWidth;
-    private int imageHeight;
 
-    public ImageCanvas(int width, int height) {
-        this.imageWidth = width;
-        this.imageHeight = height;
-    }
 
     @Override
     public void paint(final Graphics g) {
         super.paint(g);
-        this.img = new BufferedImage(imageWidth, imageHeight, TYPE_INT_RGB);
+        this.img = new BufferedImage(640, 480, TYPE_INT_RGB);
         final WritableRaster raster = img.getRaster();
         final ColorModel model = this.getColorModel();
 
-        for (i=0; i<img.getWidth(); i++) {
-            for (j=0; j<img.getHeight(); j++) {
+        for (int i=0; i<img.getWidth(); i++) {
+            for (int j=0; j<img.getHeight(); j++) {
                 raster.setDataElements(i, j, Color.BLACK);
             }
         }
-        g.drawImage(img, imageWidth, imageHeight, null);
+        g.drawImage(img, img.getWidth(), img.getHeight(), null);
     }
 
 
